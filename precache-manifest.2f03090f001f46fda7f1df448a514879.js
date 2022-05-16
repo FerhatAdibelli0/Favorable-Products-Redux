07f015f6a9f96faa43642f888eedee1c59ef807c
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-17 01:13:31 +0300

    Updates

---
+++ precache-manifest.2f03090f001f46fda7f1df448a514879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b78296bf71f91f528bc1318ba959fd",
+    "url": "/Favorable-Products-Redux/index.html"
+  },
+  {
+    "revision": "bd6ce23fcc7c6dc5a786",
+    "url": "/Favorable-Products-Redux/static/css/main.c69bebdf.chunk.css"
+  },
+  {
+    "revision": "279c609992023f7cae0b",
+    "url": "/Favorable-Products-Redux/static/js/2.db7cbc7f.chunk.js"
+  },
+  {
+    "revision": "bd6ce23fcc7c6dc5a786",
+    "url": "/Favorable-Products-Redux/static/js/main.a70d4c9a.chunk.js"
+  },
+  {
+    "revision": "f7c41ec10d8228f407f4",
+    "url": "/Favorable-Products-Redux/static/js/runtime~main.90ca494c.js"
+  }
+]);
